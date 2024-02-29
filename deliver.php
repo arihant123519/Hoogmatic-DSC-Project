f7a231cd5c35aea44e0316f5eafad3f5fa770e09
Arihant Aswal <58886962+arihant123519@users.noreply.github.com>
2024-02-29 15:24:14 +0530

    Update deliver.php

--- deliver.php
+++ deliver.php
@@ -11,5 +11,5 @@ $license_no=$_POST['license_no'];
 mysqli_query($con,"INSERT INTO delivered_dsc (awb_no, courier_comp, license_no, dsc_use_id, status) VALUES ('$awb_no', '$courier_comp', '$license_no', '$dsc_use_id', '1') ");
 mysqli_query($con,"UPDATE dsc_use SET status='2' WHERE dsc_use_id='$dsc_use_id' ");
 
-header("Location: fetch.php");
+header("Location: index.php");
 ?>
