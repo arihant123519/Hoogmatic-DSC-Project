c85bb5814952bea2fb0c350a0f58d0662bbd768b
Arihant Aswal <58886962+arihant123519@users.noreply.github.com>
2024-02-29 15:25:06 +0530

    Update status.php

--- status.php
+++ status.php
@@ -10,5 +10,5 @@ if(!empty($_GET['used'])){
     mysqli_query($con,"UPDATE dsc_use SET status='1' WHERE dsc_use_id='".$_GET['used']."' ");
 }
 
-header("Location: fetch.php");
+header("Location: index.php");
 ?>
